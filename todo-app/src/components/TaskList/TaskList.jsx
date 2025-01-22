0cb1489cc039ccbb542c94cf011b1232de9073b8
Evteshev Kirill <ergoboxvn@gmail.com>
2025-01-22 17:50:39 +0300

    todo timer

---
+++ todo-app/src/components/TaskList/TaskList.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+
+import Task from '../Task/Task'
+
+export default function TaskList(props) {
+  const { todos, deletedTask, onToggleDone, onEditTask, changeTask } = props
+  const elements = todos.map((todo) => {
+    const { id, description } = todo
+    return (
+      <Task
+        key={id}
+        todo={todo}
+        deletedTask={deletedTask}
+        onToggleDone={onToggleDone}
+        onEditTask={onEditTask}
+        changeTask={changeTask}
+        description={description}
+      />
+    )
+  })
+  return <ul className="todo-list">{elements}</ul>
+}
+
+TaskList.defaultProps = {
+  todos: [],
+}
+
+TaskList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.object),
+}
