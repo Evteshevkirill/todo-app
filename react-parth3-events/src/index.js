328b4a802d3141117a9dcf0b269464a3892fa7a1
Evteshev Kirill <ergoboxvn@gmail.com>
2025-01-22 17:54:13 +0300

    Delete react-parth3-events directory

--- react-parth3-events/src/index.js
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './components/ToDoApp/App'
-import './index.css'
-
-const root = createRoot(document.querySelector('.todoapp'))
-root.render(<App />)
